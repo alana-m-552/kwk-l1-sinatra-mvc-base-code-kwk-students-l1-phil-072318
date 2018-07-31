0c3a087c69ff9912890c9e4aaa317f46814ec873
alana-m-552 <alana.mackey0552@gmail.com>
2018-07-31 18:54:30 +0000

    automatically backed up by learn

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,14 @@
 require 'bundler'
+require './models/model'
 Bundler.require
 
 class MyApp < Sinatra::Base
-
-  get '/' do
-    erb :index
+  get  '/' do 
+    "Welcome to the Alana Lounge. Just relax and enjoy Alana's presence"
+  end 
+ 
+  get '/itslit' do 
+   erb:index   
   end
-
 end
+
